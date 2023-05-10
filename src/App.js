ec33af92046a51554d6a44cc1f40a063c80abf88
rahbarad1098 <121092804+rahbarad1098@users.noreply.github.com>
2023-05-10 15:53:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  rootElement
+);
